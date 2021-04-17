bbef57ce7e5e8af7a9fdacb7aa537b8394130084
Sean <82683346+Grue5omme@users.noreply.github.com>
2021-04-16 18:42:08 -0700

    Update randomNumber.s

--- randomNumber.s
+++ randomNumber.s
@@ -20,31 +20,6 @@ randomNumber:
 	bl	srand			@ Sets seed for srand
 
 	bl	rand			@ Generates random number
-
-	sub 	sp, fp, #4		@ Moves down one memory location from fp and stores it in sp
-	pop 	{fp, pc}		@ Pops fp and pc from the stack
-@ randomNumber.s
-
-.cpu cortex-a53
-.fpu neon-fp-armv8
-
-.data
-
-.text
-.align 2
-.global randomNumber
-.syntax unified
-.type randomNumber, %function
-
-randomNumber:
-	push	{fp, lr}		@ Pushes fp and lr onto the stack
-   	add 	fp, sp, #4		@ Adds 4 to sp and stores it in fp
-
-	mov	r0, #0			@ Stores #0 into r0
-	bl	time			@ Gets time from clock
-	bl	srand			@ Sets seed for srand
-
-	bl	rand			@ Generates random number
 	udiv	r0, r0, r9		@ Divides r0 by r9 and stores it in r0
 					 @ This avoids an error where the program takes
 					 @ such a short time to run that the system 
