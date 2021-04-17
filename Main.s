d2bb879c0b12426de07313caf8e401ef5ee620f7
Sean <82683346+Grue5omme@users.noreply.github.com>
2021-04-17 16:20:51 -0700

    Update Main.s
    Deleted a line storing the address of the first index of the array into r10. This was used for debugging purposes and is no longer necessary

--- Main.s
+++ Main.s
@@ -22,7 +22,6 @@ main:
 	mov	r0, r0, LSL #2		@ Multiplies r0 by 4 and moves that into r0
 					 @ This is the amount of memory needed
 	sub	sp, sp, r0		@ Moves the stack pointer to point to the start of the array
-	mov	r10, sp			@ Moves the stack pointer address into r10
 	str	sp, [fp, #-8]		@ Stores the stack pointer address into the first available memory address	
 	mov	r0, #11			@ Moves #11 into r0
 	str	r0, [fp, #-12]		@ Stores array size into second available memory address	
