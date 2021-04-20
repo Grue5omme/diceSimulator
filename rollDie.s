17c5351bf7f1886ac33c30d92c250700ed466dd5
Sean <82683346+SResor@users.noreply.github.com>
2021-04-19 21:16:03 -0700

    Update and rename randomNumber.s to rollDie.s

--- rollDie.s
+++ rollDie.s
@@ -1,4 +1,4 @@
-@ randomNumber.s
+@ rollDie.s
 @ This function generates a random number, modulos it with 11, adds 2 to it, and 
  @ then returns it to the calling program.
 
@@ -9,20 +9,20 @@
 
 .text
 .align 2
-.global randomNumber
+.global rollDie
 .syntax unified
-.type randomNumber, %function
+.type rollDie, %function
 
-randomNumber:
+rollDie:
 	push	{fp, lr}		@ Pushes fp and lr onto the stack
    	add 	fp, sp, #4		@ Adds 4 to sp and stores it in fp
 
 	mov	r0, #0			@ Stores #0 into r0
 
 	bl	rand			@ Generates random number
-	mov	r1, #11			@ Moves #11 into r1
+	mov	r1, #6			@ Moves #6 into r1
 	bl	modulo			@ Calls modulo function
-	add	r2, r0, #2		@ Adds #2 to number returned by modulo and moves it into r2
+	add	r0, r0, #1		@ Adds #1 to number returned by modulo and moves it into r0
 
 	sub 	sp, fp, #4		@ Moves down one memory location from fp and stores it in sp
 	pop 	{fp, pc}		@ Pops fp and pc from the stack
